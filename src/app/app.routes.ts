428d320ec7c2cba1caf092a6c0324838a1d3876e
fsanzunir <fernando.sanz305@comunidadunir.net>
2026-05-26 21:49:55 +0200

    Creación del grid

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { WelcomeComponent } from './pages/authentication/welcome-component/welcome-component';
-import {LoginComponent} from './pages/authentication/login/login.component';
+import { LoginComponent } from './pages/authentication/login/login.component';
 import { SignupComponent } from './pages/authentication/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-  { path: "", pathMatch: 'full', redirectTo: "home" },
-  { path: "home", component: WelcomeComponent },
-  { path: "login", component: LoginComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'welcome' },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'home', component: HomeComponent },
 ];
 
