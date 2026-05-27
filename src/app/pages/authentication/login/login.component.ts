a50eee7db2e5cb08ac0d30e8fa8e4cbb4999b2ce
fquincocesm-cloud <fquincocesm@gmail.com>
2026-05-27 07:01:48 +0200

    validacion

--- src/app/pages/authentication/login/login.component.ts
+++ src/app/pages/authentication/login/login.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
+  standalone: true,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
 
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      console.log('Datos listos para enviar al servidor:', this.loginForm.value);
+    }
+  }
 }
