516e44d1e7ba89d72c6d080c71dea105a1d496bb
carlesgenero-arch <carlesgenero@gmail.com>
2026-05-30 14:55:47 +0200

    creación admin-layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { WelcomeComponent } from './pages/authentication/welcome-component/welco
 import { LoginComponent } from './pages/authentication/login/login.component';
 import { SignupComponent } from './pages/authentication/signup/signup.component';
 import { UserPageInfoComponent } from './pages/user/user-page-info/user-page-info';
-import { AdminHeaderComponent } from './shared/headers/admin-header/admin-header.component';
 import { HeaderMenuComponent } from './shared/headers/header-menu/header-menu.component';
+import { AdminLayoutComponent } from './pages/admin/admin-layout/admin-layout.component';
 
 export const routes: Routes = [
   { path: "", pathMatch: 'full', redirectTo: "home" },
@@ -13,6 +13,6 @@ export const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'user-info', component: UserPageInfoComponent },
   { path: 'header-menu', component: HeaderMenuComponent},
-  { path: 'admin-header', component: AdminHeaderComponent}
+  { path: 'admin-layout', component: AdminLayoutComponent}
   
 ];
