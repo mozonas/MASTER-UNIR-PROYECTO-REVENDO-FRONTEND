689635f82a50cb2f3d0b4e7e3040ef2d76313418
Miguel <miguelozonas@gmail.com>
2026-06-01 23:29:11 +0200

    falta enrutar a donde toca, no hay componentes creados, y por eso saldrá un error en la consola

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,20 @@ import { LoginComponent } from './pages/authentication/login/login.component';
 import { SignupComponent } from './pages/authentication/signup/signup.component';
 import { UserPageInfoComponent } from './pages/user/user-page-info/user-page-info';
 import { UserPageEditComponent } from './pages/user/user-page-edit/user-page-edit';
+import { authGuard } from './guards/auth.guard';
+
+//import { roleGuard } from './guards/role.guard';
+
 
 export const routes: Routes = [
   { path: "", pathMatch: 'full', redirectTo: "home" },
   { path: "home", component: WelcomeComponent },
   { path: "login", component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'user-info', component: UserPageInfoComponent },
-  { path: 'user-edit', component: UserPageEditComponent }
+  { path: 'user-info', component: UserPageInfoComponent, canActivate: [authGuard] },
+  { path: 'user-edit', component: UserPageEditComponent, canActivate: [authGuard] }
+
+  // A medida que se vayan creando los componentes de cada página, se irán añadiendo aquí con su correspondiente path y guard si es necesario
+  //Habrá incluso rutas para cada rol, por ejemplo, /admin, /moderador, /usuario, etc. y se protegerán con el guard correspondiente para cada rol
+  //Usaremos la línea comentada roleGuard
 ];
