1d3d91a5f493130caccb839f2c9a1ba930b1042a
David Rodríguez Marbán <davidrodriguezmarban@gmail.com>
2026-06-02 14:43:08 +0200

    Fix: Errores en develop

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,46 +1,20 @@
-import { Routes } from '@angular/router';
-import { WelcomeComponent } from './pages/authentication/welcome-component/welcome-component';
-import { LoginComponent } from './pages/authentication/login/login.component';
-import { SignupComponent } from './pages/authentication/signup/signup.component';
-import { UserPageInfoComponent } from './pages/user/user-page-info/user-page-info';
-import { UserPageEditComponent } from './pages/user/user-page-edit/user-page-edit';
-import { authGuard } from './guards/auth.guard';
-import { UserPageSell } from './pages/user/user-page-sell/user-page-sell';
-import { ArticleDetailComponent } from './pages/article-detail-component/article-detail-component';
-import { AdminHeaderComponent } from './shared/headers/admin-header/admin-header.component';
-import { HeaderMenuComponent } from './shared/headers/header-menu/header-menu.component';
-
-import { roleGuard } from './guards/role.guard';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 
-export const routes: Routes = [
-  { path: "", pathMatch: 'full', redirectTo: "home" },
-  { path: "home", component: WelcomeComponent },
-  { path: "login", component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'user-info', component: UserPageInfoComponent, canActivate: [authGuard] },
-  { path: 'user-edit', component: UserPageEditComponent, canActivate: [authGuard] },
-  { path: 'user-sell', component: UserPageSell, canActivate: [authGuard] },
-  { path: 'article-detail', component: ArticleDetailComponent },
-  { path: 'header-menu', component: HeaderMenuComponent},
-  { path: 'admin-header', component: AdminHeaderComponent, canActivate: [roleGuard(['admin'])] }
+import { routes } from './app.routes';
 
-  // A medida que se vayan creando los componentes de cada página, se irán añadiendo aquí con su correspondiente path y guard si es necesario
-  //Habrá incluso rutas para cada rol, por ejemplo, /admin, /moderador, /usuario, etc. y se protegerán con el guard correspondiente para cada rol
-  //Usaremos la línea comentada roleGuard
-
-  /*
-  Cómo usarlo:
-  {
-    path: 'admin',
-    canActivate: [roleGuard(['admin'])],
-    loadComponent: () => import('./pages/admin/admin.component')
-  }
-
-  {
-    path: 'moderador',
-    canActivate: [roleGuard(['moderador'])],
-    loadComponent: () => import('./pages/moderador/moderador.component')
-  }
-  */
-];
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+        // interceptor para añadir el token JWT a las cabeceras de las peticiones HTTP
+    {
+      provide: HTTP_INTERCEPTORS,
+      useValue: authInterceptor,
+      multi: true
+    }
+  ]
+};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,43 @@
+// import { Routes } from '@angular/router';
+// import { WelcomeComponent } from './pages/authentication/welcome-component/welcome-component';
+// import { LoginComponent } from './pages/authentication/login/login.component';
+// import { SignupComponent } from './pages/authentication/signup/signup.component';
+// import { UserPageInfoComponent } from './pages/user/user-page-info/user-page-info';
+// import { UserPageEditComponent } from './pages/user/user-page-edit/user-page-edit';
+// import { UserPageSell } from './pages/user/user-page-sell/user-page-sell';
+// import { ArticleDetailComponent } from './pages/article-detail-component/article-detail-component';
+// import { AdminHeaderComponent } from './shared/headers/admin-header/admin-header.component';
+// import { HeaderMenuComponent } from './shared/headers/header-menu/header-menu.component';
+
+// //import { roleGuard } from './guards/role.guard';
+
+
+// export const routes: Routes = [
+//   { path: "", pathMatch: 'full', redirectTo: "home" },
+//   { path: "home", component: WelcomeComponent },
+//   { path: "login", component: LoginComponent },
+//   { path: 'signup', component: SignupComponent },
+//   { path: 'user-info', component: UserPageInfoComponent },
+//   { path: 'user-edit', component: UserPageEditComponent },
+//   { path: 'user-sell', component: UserPageSell },
+//   { path: 'article-detail', component: ArticleDetailComponent },
+//   { path: 'header-menu', component: HeaderMenuComponent},
+//   { path: 'admin-header', component: AdminHeaderComponent}
+// ];
+
 import { Routes } from '@angular/router';
 import { WelcomeComponent } from './pages/authentication/welcome-component/welcome-component';
 import { LoginComponent } from './pages/authentication/login/login.component';
 import { SignupComponent } from './pages/authentication/signup/signup.component';
 import { UserPageInfoComponent } from './pages/user/user-page-info/user-page-info';
 import { UserPageEditComponent } from './pages/user/user-page-edit/user-page-edit';
+import { authGuard } from './guards/auth.guard';
 import { UserPageSell } from './pages/user/user-page-sell/user-page-sell';
 import { ArticleDetailComponent } from './pages/article-detail-component/article-detail-component';
 import { AdminHeaderComponent } from './shared/headers/admin-header/admin-header.component';
 import { HeaderMenuComponent } from './shared/headers/header-menu/header-menu.component';
 
-//import { roleGuard } from './guards/role.guard';
+import { roleGuard } from './guards/role.guard';
 
 
 export const routes: Routes = [
@@ -17,10 +45,29 @@ export const routes: Routes = [
   { path: "home", component: WelcomeComponent },
   { path: "login", component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'user-info', component: UserPageInfoComponent },
-  { path: 'user-edit', component: UserPageEditComponent },
-  { path: 'user-sell', component: UserPageSell },
+  { path: 'user-info', component: UserPageInfoComponent, canActivate: [authGuard] },
+  { path: 'user-edit', component: UserPageEditComponent, canActivate: [authGuard] },
+  { path: 'user-sell', component: UserPageSell, canActivate: [authGuard] },
   { path: 'article-detail', component: ArticleDetailComponent },
   { path: 'header-menu', component: HeaderMenuComponent},
-  { path: 'admin-header', component: AdminHeaderComponent}
+  { path: 'admin-header', component: AdminHeaderComponent, canActivate: [roleGuard(['admin'])] }
+
+  // A medida que se vayan creando los componentes de cada página, se irán añadiendo aquí con su correspondiente path y guard si es necesario
+  //Habrá incluso rutas para cada rol, por ejemplo, /admin, /moderador, /usuario, etc. y se protegerán con el guard correspondiente para cada rol
+  //Usaremos la línea comentada roleGuard
+
+  /*
+  Cómo usarlo:
+  {
+    path: 'admin',
+    canActivate: [roleGuard(['admin'])],
+    loadComponent: () => import('./pages/admin/admin.component')
+  }
+
+  {
+    path: 'moderador',
+    canActivate: [roleGuard(['moderador'])],
+    loadComponent: () => import('./pages/moderador/moderador.component')
+  }
+  */
 ];
