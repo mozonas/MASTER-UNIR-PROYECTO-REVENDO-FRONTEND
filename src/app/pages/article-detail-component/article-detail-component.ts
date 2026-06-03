cb1f56ca8b59b45751e55bbe4edbeb0e1bba3dae
Inés Carazo Núñez <inescnunez@gmail.com>
2026-06-03 20:14:45 +0200

    Nuevo carrusel

--- src/app/pages/article-detail-component/article-detail-component.ts
+++ src/app/pages/article-detail-component/article-detail-component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { HeaderMenuComponent } from "../../shared/headers/header-menu/header-menu.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-article-detail-component',
-  imports: [HeaderMenuComponent],
+  imports: [HeaderMenuComponent, CommonModule],
   templateUrl: './article-detail-component.html',
   styleUrl: './article-detail-component.css',
 })
 export class ArticleDetailComponent {
+  modalOpen = false;
+  selectedImage: string | null = null;
 
+  openImageModal(img: string) {
+    this.selectedImage = img;
+    this.modalOpen = true;
+  }
+
+  closeModal() {
+    this.modalOpen = false;
+    this.selectedImage = null;
+  }
 }
