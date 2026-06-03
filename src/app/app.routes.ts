cee4fa114ace108f85467e539dcac6fe10d9b544
David Rodríguez Marbán <davidrodriguezmarban@gmail.com>
2026-06-03 14:33:10 +0200

    Fix: routes y ejemplo de uso

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,3 @@
-// import { Routes } from '@angular/router';
-// import { WelcomeComponent } from './pages/authentication/welcome-component/welcome-component';
-// import { LoginComponent } from './pages/authentication/login/login.component';
-// import { SignupComponent } from './pages/authentication/signup/signup.component';
-// import { UserPageInfoComponent } from './pages/user/user-page-info/user-page-info';
-// import { UserPageEditComponent } from './pages/user/user-page-edit/user-page-edit';
-// import { UserPageSell } from './pages/user/user-page-sell/user-page-sell';
-// import { ArticleDetailComponent } from './pages/article-detail-component/article-detail-component';
-// import { AdminHeaderComponent } from './shared/headers/admin-header/admin-header.component';
-// import { HeaderMenuComponent } from './shared/headers/header-menu/header-menu.component';
-
-// //import { roleGuard } from './guards/role.guard';
-
-
-// export const routes: Routes = [
-//   { path: "", pathMatch: 'full', redirectTo: "home" },
-//   { path: "home", component: WelcomeComponent },
-//   { path: "login", component: LoginComponent },
-//   { path: 'signup', component: SignupComponent },
-//   { path: 'user-info', component: UserPageInfoComponent },
-//   { path: 'user-edit', component: UserPageEditComponent },
-//   { path: 'user-sell', component: UserPageSell },
-//   { path: 'article-detail', component: ArticleDetailComponent },
-//   { path: 'header-menu', component: HeaderMenuComponent},
-//   { path: 'admin-header', component: AdminHeaderComponent}
-// ];
-
 import { Routes } from '@angular/router';
 import { WelcomeComponent } from './pages/authentication/welcome-component/welcome-component';
 import { LoginComponent } from './pages/authentication/login/login.component';
