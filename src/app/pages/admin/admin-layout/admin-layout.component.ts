97e9419565ea7df86337d5323940cc2b2f5e0c7b
carlesgenero-arch <carlesgenero@gmail.com>
2026-06-05 10:26:43 +0200

    Separar header y aside en shared components

--- src/app/pages/admin/admin-layout/admin-layout.component.ts
+++ src/app/pages/admin/admin-layout/admin-layout.component.ts
@@ -1,19 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
 import { AdminDashboardComponent } from '../admin-dashboard/admin-dashboard.component';
+import { AdminHeaderComponent } from '../../../shared/headers/admin-header/admin-header.component';
+import { AsideComponent } from '../../../shared/aside/aside.component';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterLink, AdminDashboardComponent],
+  imports: [AdminDashboardComponent,AdminHeaderComponent,AsideComponent],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.css',
 })
 export class AdminLayoutComponent {
 
-  actividadOpen = false;
-
-  desplegarMenu(){
-    this.actividadOpen = !this.actividadOpen;
-  }
-
+  
 }
