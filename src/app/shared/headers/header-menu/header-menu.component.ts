9b562d0dbe63a533b08ab66c9c4bc92ec5491759
Inés Carazo Núñez <inescnunez@gmail.com>
2026-06-11 20:10:50 +0200

    Merge branch 'feature/unifyingStyles' into develop

--- src/app/shared/headers/header-menu/header-menu.component.ts
+++ src/app/shared/headers/header-menu/header-menu.component.ts
@@ -1,8 +1,6 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, inject, Input, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
-import { Component, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header-menu',
@@ -11,12 +9,30 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header-menu.component.css',
 })
 export class HeaderMenuComponent {
+
+  
  @Input() mostrarMenu: boolean = true;
+
+  //inyección servicio para seleccionar role
   private authService = inject (AuthService); 
 
   // Inyectamos el Router para la redirección post-logout
   private router = inject(Router);
 
   logout(): void {
+    // Borra el token del sessionStorage (ajusta 'token' al nombre exacto que uses)
     sessionStorage.removeItem('token');
+
+    // Opcional, si quieremos limpiar TODA la sesión:
+    // sessionStorage.clear();
+
+    // Redirige al usuario a la página principal o al login
+    this.router.navigate(['/']);
+  }
+
+    role = signal(this.authService.getUserRole() ?? 'USUARIO');
+    username = signal(this.authService.getUserName() ?? '');
+
+    isStaff = computed(() => ['ADMIN', 'MODERADOR'].includes(this.role()));
+
 }
